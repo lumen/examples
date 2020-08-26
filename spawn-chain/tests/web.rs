8c64ae41f7e872a5941e476ff8c651ec5816a51a
Luke Imhoff <Kronic.Deth@gmail.com>
2020-08-25 21:36:52 -0500

    Fix "no tests to run" for wasm-pack run
    Our versions of wasm-pack and wasm-bindgen got out-of-sync, so that the test runner couldn't find the tests anymore, so update all the wasm dependencies to fix it.
    
    As `async` is stable in Rust now, switch from `wasm_bindgen_test(async)` to `wasm_bindgen_test` on an `async` function.  This eliminates the need for the `futures` crate and is more up-to-date with the community best practices.

--- spawn-chain/tests/web.rs
+++ spawn-chain/tests/web.rs
@@ -5,8 +5,6 @@ extern crate wasm_bindgen_test;
 
 use std::sync::Once;
 
-use futures::future::Future;
-
 use wasm_bindgen::{JsCast, JsValue};
 
 use wasm_bindgen_futures::JsFuture;
@@ -24,33 +22,33 @@ mod run {
 
     use spawn_chain::run;
 
-    #[wasm_bindgen_test(async)]
-    fn with_1() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(1)
+    #[wasm_bindgen_test]
+    async fn with_1() {
+        eq(1).await;
     }
 
-    #[wasm_bindgen_test(async)]
-    fn with_2() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(2)
+    #[wasm_bindgen_test]
+    async fn with_2() {
+        eq(2).await;
     }
 
-    #[wasm_bindgen_test(async)]
-    fn with_4() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(4)
+    #[wasm_bindgen_test]
+    async fn with_4() {
+        eq(4).await;
     }
 
-    #[wasm_bindgen_test(async)]
-    fn with_8() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(8)
+    #[wasm_bindgen_test]
+    async fn with_8() {
+        eq(8).await;
     }
 
-    #[wasm_bindgen_test(async)]
-    fn with_16() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(16)
+    #[wasm_bindgen_test]
+    async fn with_16() {
+        eq(16).await;
     }
 
-    fn eq_in_the_future(n: usize) -> impl Future<Item = (), Error = JsValue> {
-        super::eq_in_the_future(run, n)
+    async fn eq(n: usize) {
+        super::eq(run, n).await;
     }
 }
 
@@ -59,33 +57,33 @@ mod log_to_console {
 
     use spawn_chain::log_to_console;
 
-    #[wasm_bindgen_test(async)]
-    fn with_1() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(1)
+    #[wasm_bindgen_test]
+    async fn with_1() {
+        eq(1).await;
     }
 
-    #[wasm_bindgen_test(async)]
-    fn with_2() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(2)
+    #[wasm_bindgen_test]
+    async fn with_2() {
+        eq(2).await;
     }
 
-    #[wasm_bindgen_test(async)]
-    fn with_4() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(4)
+    #[wasm_bindgen_test]
+    async fn with_4() {
+        eq(4).await;
     }
 
-    #[wasm_bindgen_test(async)]
-    fn with_8() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(8)
+    #[wasm_bindgen_test]
+    async fn with_8() {
+        eq(8).await;
     }
 
-    #[wasm_bindgen_test(async)]
-    fn with_16() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(16)
+    #[wasm_bindgen_test]
+    async fn with_16() {
+        eq(16).await;
     }
 
-    fn eq_in_the_future(n: usize) -> impl Future<Item = (), Error = JsValue> {
-        super::eq_in_the_future(log_to_console, n)
+    async fn eq(n: usize) {
+        super::eq(log_to_console, n).await;
     }
 }
 
@@ -94,69 +92,63 @@ mod log_to_dom {
 
     use spawn_chain::log_to_dom;
 
-    #[wasm_bindgen_test(async)]
-    fn with_1() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(1)
+    #[wasm_bindgen_test]
+    async fn with_1() {
+        eq(1).await;
     }
 
-    #[wasm_bindgen_test(async)]
-    fn with_2() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(2)
+    #[wasm_bindgen_test]
+    async fn with_2() {
+        eq(2).await;
     }
 
-    #[wasm_bindgen_test(async)]
-    fn with_4() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(4)
+    #[wasm_bindgen_test]
+    async fn with_4() {
+        eq(4).await;
     }
 
-    #[wasm_bindgen_test(async)]
-    fn with_8() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(8)
+    #[wasm_bindgen_test]
+    async fn with_8() {
+        eq(8).await;
     }
 
-    #[wasm_bindgen_test(async)]
-    fn with_16() -> impl Future<Item = (), Error = JsValue> {
-        eq_in_the_future(16)
+    #[wasm_bindgen_test]
+    async fn with_16() {
+        eq(16).await;
     }
 
-    fn eq_in_the_future(n: usize) -> impl Future<Item = (), Error = JsValue> {
-        super::eq_in_the_future(log_to_dom, n)
+    async fn eq(n: usize) {
+        super::eq(log_to_dom, n).await;
     }
 }
 
 static START: Once = Once::new();
 
-fn eq_in_the_future(
-    f: fn(usize) -> js_sys::Promise,
-    n: usize,
-) -> impl Future<Item = (), Error = JsValue> {
+async fn eq(f: fn(usize) -> js_sys::Promise, n: usize) {
     start_once();
 
     let promise = f(n);
+    let resolved = JsFuture::from(promise).await.unwrap();
 
-    JsFuture::from(promise)
-        .map(move |resolved| {
-            assert!(
-                js_sys::Array::is_array(&resolved),
-                "{:?} is not an array",
-                resolved
-            );
+    assert!(
+        js_sys::Array::is_array(&resolved),
+        "{:?} is not an array",
+        resolved
+    );
 
-            let resolved_array: js_sys::Array = resolved.dyn_into().unwrap();
+    let resolved_array: js_sys::Array = resolved.dyn_into().unwrap();
 
-            assert_eq!(resolved_array.length(), 2);
+    assert_eq!(resolved_array.length(), 2);
 
-            let resolved_time = Reflect::get(&resolved_array, &0.into()).unwrap();
+    let resolved_time = Reflect::get(&resolved_array, &0.into()).unwrap();
 
-            assert!(js_sys::Number::is_integer(&resolved_time));
+    assert!(js_sys::Number::is_integer(&resolved_time));
 
-            let n_js_value: JsValue = (n as i32).into();
-            assert_eq!(
-                Reflect::get(&resolved_array, &1.into()).unwrap(),
-                n_js_value
-            );
-        })
-        .map_err(|_| unreachable!())
+    let n_js_value: JsValue = (n as i32).into();
+    assert_eq!(
+        Reflect::get(&resolved_array, &1.into()).unwrap(),
+        n_js_value
+    );
 }
 
 fn start_once() {
